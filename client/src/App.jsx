7265e3a19f05e76b638ffb91f869f87f4ea7128c
Beatriz Rodríguez <159242055+BeatrizRodriguezReglero@users.noreply.github.com>
2024-11-08 11:04:58 +0100

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Users from './components/users/Users';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyles />
+			<Users/>
+		</>
+	);
+};
+
+export default App;
